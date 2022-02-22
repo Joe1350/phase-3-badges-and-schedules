33b99896827c611928a30c862f62f9ccc87c660f
Joseph Martin <josephmartin1350@gmail.com>
2022-02-21 22:02:58 -0700

    commit tree 211b808227d9288f5522d452a363fb9357bcd747

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+    names.map { |name| badge_maker(name)}
+end
+
+def assign_rooms names
+    names.map.with_index(1) { |name, index| "Hello, #{name}! You'll be assigned to room #{index}!" }
+end
+
+def printer names
+    batch_badge_creator(names).each { |badge| puts badge }
+    assign_rooms(names).each { |assigned_room| puts assigned_room }
+end
